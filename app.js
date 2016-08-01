47018fa79ed70d8ead56bd4eacaa45f4cb7739d5
godiagonal <same_johansson@hotmail.com>
2016-08-02 01:14:05 +0200

    ただいまー

---
+++ app.js
@@ -0,0 +1,53 @@
+/* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
+var allowedOrigins = "http://localhost:* http://127.0.0.1:*",
+    app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server, {
+        origins: allowedOrigins
+    })
+port = 8080,
+    url = 'http://localhost:' + port + '/';
+
+server.listen(port);
+console.log("Express server listening on port " + port);
+console.log(url);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+    console.log(socket.id);
+
+    socket.emit('nationList', {
+        nations: [
+            {id: 1, name: 'Gotland', location: {latitude: 59.8597109, longitude: 17.6335796}},
+            {id: 2, name: 'Stockholm', location: {latitude: 59.8555954, longitude: 17.637574}},
+        ]
+    });
+
+    setTimeout(function () {
+        socket.emit('addNation', {
+            nation: {id: 3, name: 'nation 3', location: {latitude: 59.859, longitude: 17.637574}},
+        });
+    }, 1000);
+
+    setTimeout(function () {
+        socket.emit('updateNation', {
+            nation: {id: 3, name: 'nation 4'},
+        });
+    }, 3000);
+
+    setTimeout(function () {
+        socket.emit('removeNation', {
+            nation: {id: 3},
+        });
+    }, 4000);
+
+    socket.on('pongo', function (data) {
+        console.log(data);
+    });
+
+})
+;
