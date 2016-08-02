fe6721a1b98923d352c1525c347d9efaf3c521d9
godiagonal <same_johansson@hotmail.com>
2016-08-03 01:12:27 +0200

    Changed some mock-nations to include name slugs.

--- app.js
+++ app.js
@@ -11,33 +11,55 @@ server.listen(port);
 console.log('Express server listening on port ' + port);
 console.log(url);
 
+const nations = [
+    {
+        id: 1, name: 'Gotland', currentVisitors: 9, maxVisitors: 10,
+        location: {latitude: 59.8597109, longitude: 17.6335796},
+        slug: 'gotland'
+    },
+    {
+        id: 2, name: 'Stockholm', currentVisitors: 2, maxVisitors: 10,
+        location: {latitude: 59.8555954, longitude: 17.637574},
+        slug: 'stockholm'
+    },
+    {
+        id: 3, name: 'Upland', currentVisitors: 5, maxVisitors: 10,
+        location: {latitude: 59.859, longitude: 17.637574},
+        slug: 'upland'
+    },
+];
+
 io.sockets.on('connection', function (socket) {
 
     console.log('New connection:' + socket.id);
 
     socket.emit('nationList', {
-        nations: [
-            {id: 1, name: 'Gotland', location: {latitude: 59.8597109, longitude: 17.6335796}},
-            {id: 2, name: 'Stockholm', location: {latitude: 59.8555954, longitude: 17.637574}},
-        ]
+        nations: nations.slice(0,2)
     });
 
     setTimeout(function () {
         socket.emit('addNation', {
-            nation: {id: 3, name: 'nation 3', location: {latitude: 59.859, longitude: 17.637574}},
+            nation: nations[2]
         });
     }, 1000);
 
     setTimeout(function () {
         socket.emit('updateNation', {
-            nation: {id: 3, name: 'nation 4'},
+            nation: {
+                id: 3,
+                name: 'Upland',
+                location: {latitude: 59.859, longitude: 17.637574},
+                currentVisitors: 7,
+                maxVisitors: 10,
+                slug: 'upland'
+            }
         });
     }, 3000);
 
     setTimeout(function () {
-        socket.emit('removeNation', {
-            nation: {id: 3},
-        });
+        /*socket.emit('removeNation', {
+         nation: {id: 3},
+         });*/
     }, 4000);
 
     socket.on('ping', function (data) {
