d302ea76acf66a29b0c9ac6922d8b50a8f680edc
godiagonal <same_johansson@hotmail.com>
2016-08-13 18:24:45 +0200

    Hehe

--- app/config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'mongodb://public:diEneds8argypoto*@ds029655.mlab.com:29655/nations-db'
-}
--- app/config/facebook.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    apiVersion: '2.7',
-    accessToken: '1772899526323726|f00c20c0046f9e0d117ba1460f6f8edd',
-    placeCacheExpires: 43200, // 12 h
-    eventsCacheExpires: 43200, // 12 h
-    eventUrl: 'https://www.facebook.com/events/'
-}
