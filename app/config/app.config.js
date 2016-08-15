9f74146df7a50fb5a20c829155388aa05b7b30eb
godiagonal <same_johansson@hotmail.com>
2016-08-15 23:20:36 +0200

    Deplying for production

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     socketOptions: {
-        origins: 'http://localhost:* http://127.0.0.1:*'
+        origins: 'http://nations.c2h4imtcm2.eu-west-1.elasticbeanstalk.com:* http://localhost:* http://127.0.0.1:*'
     },
     apiOptions: {
         origins: [
             'http://localhost:4200',
-            'http://godiagonal.com'
+            'http://godiagonal.com',
+            'http://nations.godiagonal.com'
         ]
     },
     defaultTimezone: 'Europe/Stockholm'
